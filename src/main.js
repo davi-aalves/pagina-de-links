0a5be1cff9278b64272cd1b3257a29a049516c45
Davi A Alves <daviih2000@gmail.com>
2025-02-27 17:59:09 -0300

    fix: correct script path in index.html; update profile name and avatar in profile.json; fix variable name in main.js; enhance card styles in style.css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import profileData from "/profile.json";
 document.title = "Links de " + profileData.name + " " + profileData.lastName;
 
 window.onload = function () {
-  const avatarImage = domuent.querySelector(".card-header__profile-image");
+  const avatarImage = document.querySelector(".card-header__profile-image");
   const name = document.querySelector(".card-header__name");
   const lastName = document.querySelector(".card-header__last-name");
   const description = document.querySelector(".card-header__description");
@@ -16,7 +16,7 @@ window.onload = function () {
     "Imagem de perfil de " + profileData.name + " " + profileData.lastName;
 
   lastName.textContent = profileData.lastName;
-  name.textContent = profileData.lastName;
+  name.textContent = profileData.name;
   name.appendChild(lastName);
 
   description.textContent = profileData.description;
@@ -28,8 +28,22 @@ window.onload = function () {
     a.href = link.href;
     a.textContent = link.text;
     a.target = "_blank";
-
     li.appendChild(a);
     linksList.appendChild(li);
+  });
+
+  profileData.socialLinks.forEach((socialLink) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    img.src = socialLink.icon;
+    img.alt = socialLink.text;
+    a.href = socialLink.href;
+    a.target = "_blank";
+
+    a.appendChild(img);
+    li.appendChild(a);
+    socialLinks.appendChild(li);
   });
 };
